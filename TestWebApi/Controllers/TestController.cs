f216cbd2de363e0e07ffa9ec11981d2d84b575dc
sudhanshu-verma <sudhanshu.verma@hotmail.com>
2018-08-07 08:36:25 -0500

    changed message

---
+++ TestWebApi/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestWebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        [Route("test")]
+        public async Task<IHttpActionResult> TestMethod()
+        {   
+            return Ok("Working absolutely fine");
+        }        
+    }
+}
